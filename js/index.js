afc5fdb68d8d0c6955dca11f1cac998ad46e68e2
Aman Sahu <amansahu9000@gmail.com>
2024-12-30 15:34:28 -0500

    changed styling of home

--- js/index.js
+++ js/index.js
@@ -10,3 +10,23 @@ function loadContent(url) {
         iframe.style.display = 'block';
     };
 }
+
+// function changeNavbarColor(page) {
+//     var navbar = document.getElementById('navbar');
+//     switch(page) {
+//         case 'home':
+//             navbar.style.borderBottomColor = 'blue';
+//             break;
+//         case 'mission':
+//             navbar.style.borderBottomColor = 'green';
+//             break;
+//         case 'concept':
+//             navbar.style.borderBottomColor = 'purple';
+//             break;
+//         case 'contact':
+//             navbar.style.borderBottomColor = 'red';
+//             break;
+//         default:
+//             navbar.style.borderBottomColor = 'grey';
+//     }
+// }
